49fd10e9b6c22161331e60279c0c01c9c95c185a
Josje1993 <josje-vt-padje@hotmail.com>
2016-10-26 19:44:13 +0200

    Split the header and the side navbar into a separate file for easy working

--- www/rially/android/dashboard/pages/add_modifier.php
+++ www/rially/android/dashboard/pages/add_modifier.php
@@ -97,84 +97,11 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['modifier'])) {
 
     <!-- Navigation -->
     <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php">Rially Admin</a>
-        </div>
-        <!-- /.navbar-header -->
 
-        <ul class="nav navbar-top-links navbar-right">
+        <?php include "page_parts/header.html"?>
 
-            <!-- /.dropdown -->
-            <li class="dropdown">
-                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                    <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
-                </a>
-                <ul class="dropdown-menu dropdown-user">
-                    <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                    </li>
-                    <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                    </li>
-                    <li class="divider"></li>
-                    <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                    </li>
-                </ul>
-                <!-- /.dropdown-user -->
-            </li>
-            <!-- /.dropdown -->
-        </ul>
-        <!-- /.navbar-top-links -->
+        <?php include "page_parts/navbar.html" ?>
 
-        <div class="navbar-default sidebar" role="navigation">
-            <div class="sidebar-nav navbar-collapse">
-                <ul class="nav" id="side-menu">
-                    <li class="sidebar-search">
-                        <div class="input-group custom-search-form">
-                            <input type="text" class="form-control" placeholder="Search...">
-                            <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                        </div>
-                        <!-- /input-group -->
-                    </li>
-                    <li>
-                        <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="add_user.php"><i class="fa fa-users fa-fw"></i> Add User/Team</a>
-                    </li>
-                    <li>
-                        <a href="add_assignment.php"><i class="fa fa-list fa-fw"></i> Add Assignments</a>
-                    </li>
-                    <li>
-                        <a href="add_modifier.php"><i class="fa fa-list fa-fw"></i> Add Modifiers</a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-user fa-fw"></i> Participants<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <?php
-                            $result = mysqli_query($db, "SELECT uid,username FROM users");
-
-                            while ($row = mysqli_fetch_array($result)) {
-                                $username = $row["username"];
-                                $uid = $row["uid"];
-                                echo "<li> <a href='viewimages.php?username=$username'>$username</a></li>";
-                            }
-                            ?>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.sidebar-collapse -->
-        </div>
-        <!-- /.navbar-static-side -->
     </nav>
 
     <div id="page-wrapper">
