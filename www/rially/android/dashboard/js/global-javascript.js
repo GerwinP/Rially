f13c3274be3c4a297b3f8dcb9d1135cd3d061f8f
GERWINPC\Gerwin <gerwinputtenstein@gmail.com>
2017-03-28 14:50:29 +0200

    Did some styling to the pages in way of using some different colors, conform the Inter-Actief corporate identity

--- www/rially/android/dashboard/js/global-javascript.js
+++ www/rially/android/dashboard/js/global-javascript.js
@@ -10,17 +10,20 @@ $header.load("page_parts/header.html");
 $navbar.load("page_parts/navbar.html");
 
 $(function() {
-   
-    var $participants = $('#participants');
 
     $.ajax({
        type: 'GET',
         url: '../../get_all_users.php',
         success: function(usersReturn) {
             var users = JSON.parse(usersReturn);
+            console.log("Buidling");
             $.each(users.users, function(i, user) {
-               $participants.append("<li> <a href='viewimages.php?username=" + user.username +"'>" + user.username + "</a></li>")
+                $('#participants').append("<li> <a href='viewimages.php?username=" + user.username +"'>" + user.username + "</a></li>")
             });
         }
+    });
+
+    $("a").mouseup(function() {
+        $(this).blur();
     });
 });
